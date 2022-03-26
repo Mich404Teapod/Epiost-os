208c695a9570129244418fef637acd8f6618ef40
Mich404Teapod <101505439+Mich404Teapod@users.noreply.github.com>
2022-03-26 12:35:42 +0100

    Delete applications directory

--- applications/teapod-drive/folder/viewfolder.js
+++
@@ -1,6 +0,0 @@
-(function () {
-  let crossbtn = document.querySelector("#closebtn");
-  crossbtn.addEventListener("click", function () {
-    document.getElementById("view").classList.remove("active");
-  });
-})();
--- applications/teapod-drive/index.js
+++
@@ -1,3 +0,0 @@
-let resources = [];
-let currentfolderid = -1; // initially we are at root (which has an id of -1)
-let resourceid = 0;
